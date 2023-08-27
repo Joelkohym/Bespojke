2941163a9048c0c4aea172c263ff43bfeed3b382
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-08-27 14:14:43 +0000

    Forked https://github.com/aakashns/jovian-careers-website

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+babyProducts = [{
+  'id': 1,
+  'title': '10 Pcs Silicone Dining Set',
+  'Price': '$27.90',
+  'Description': 'For babies'
+}, {
+  'id': 2,
+  'title': '10 Pcs Silicone Dining Set',
+  'Price': '$27.90',
+  'Description': 'For babies'
+}, {
+  'id': 3,
+  'title': '10 Pcs Silicone Dining Set',
+  'Price': '$27.90',
+  'Description': 'For babies'
+}]
+
+
+@app.route("/")
+def hello_jovian():
+  return render_template('home.html',
+                         products=babyProducts,
+                         company_name='Bespojke')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(babyProducts)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
