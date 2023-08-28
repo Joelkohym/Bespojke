f32e2f5e6d74ff4b2b80e5fca3b7fd54b8da767a
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-08-28 17:10:44 +0000

    csv cities

--- app.py
+++ app.py
@@ -141,41 +141,22 @@ def Vessel_movement_receive(formName=None):
 
 @app.route("/api/vessel/receive/get")
 def VMR_GET():
-  st.set_page_config(layout="wide")
-  st.sidebar.info(
-      """
-      - Web App URL: <https://streamlit.geemap.org>
-      - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
-      """
-  )
-  st.sidebar.title("Contact")
-  st.sidebar.info(
-      """
-      Qiusheng Wu: <https://wetlands.io>
-      [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
-      """
-  )
-  st.title("Marker Cluster")
-  with st.expander("See source code"):
-      with st.echo():
-  
-          m = leafmap.Map(center=[40, -100], zoom=4)
-          cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-          regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
-  
-          m.add_geojson(regions, layer_name='US Regions')
-          m.add_points_from_xy(
-              cities,
-              x="longitude",
-              y="latitude",
-              color_column='region',
-              icon_names=['gear', 'map', 'leaf', 'globe'],
-              spin=True,
-              add_legend=True,
-          )
-  
-          m.to_streamlit(height=700)
+  m = leafmap.Map(center=[40, -100], zoom=4)
+  cities = '/static/sg_cities.csv'
+  regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
 
+  m.add_geojson(regions, layer_name='US Regions')
+  m.add_points_from_xy(
+      cities,
+      x="longitude",
+      y="latitude",
+      color_column='region',
+      icon_names=['gear', 'map', 'leaf', 'globe'],
+      spin=True,
+      add_legend=True,
+  )
+  m.to_html("mymap.html")
+  return render_template('mymap.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
