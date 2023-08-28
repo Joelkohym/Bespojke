9174f7b447401fe44d9295be20eb06c95a4e63c0
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-08-28 01:35:52 +0000

    added SGTD

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify
+import http.client
+
 
 app = Flask(__name__)
 
@@ -7,17 +9,18 @@ babyProducts = [{
   'title': '10 Pcs Silicone Dining Set',
   'price': '$27.90',
   'description': 'For babies'
-}, {
-  'id': 2,
-  'title': '10 Pcs Silicone Dining Set',
-  'price': '$27.90',
-  'description': 'For babies'
-}, {
-  'id': 3,
-  'title': '10 Pcs Silicone Dining Set',
-  'price': '$27.90',
-  'description': 'For babies'
 }]
+# }, {
+#   'id': 2,
+#   'title': '10 Pcs Silicone Dining Set',
+#   'price': '$27.90',
+#   'description': 'For babies'
+# }, {
+#   'id': 3,
+#   'title': '10 Pcs Silicone Dining Set',
+#   'price': '$27.90',
+#   'description': 'For babies'
+# }]
 
 
 @app.route("/")
@@ -30,6 +33,20 @@ def hello_jovian():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(babyProducts)
+
+@app.route("/api/SGTD")
+def SGTD():
+  conn =     http.client.HTTPSConnection('bespojke.com/api/sgtd')
+  conn.request("POST", "/", '''{
+  "test": "event"
+}''', {'Content-Type': 'application/json'})
+  # Get the HTTP response
+  connResponse = conn.getresponse();
+# Read the HTTP response
+  response     = connResponse.read();
+# Print the HTTP response
+  print(response);
+  return jsonify(response)
 
 
 if __name__ == '__main__':
