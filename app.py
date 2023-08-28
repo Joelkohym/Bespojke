b47ff749650c8b0c9caa93c2c0fcf78f32faa1f5
Joelkohym <56166111+Joelkohym@users.noreply.github.com>
2023-08-28 17:38:20 +0000

    geojson

--- app.py
+++ app.py
@@ -142,15 +142,15 @@ def Vessel_movement_receive(formName=None):
 @app.route("/api/vessel/receive/get")
 def VMR_GET():
   m = leafmap.Map(center=[40, -100], zoom=4)
-  cities = '/static/sg_cities.csv'
-  regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+  cities = 'static/sg_cities.csv'
+  regions = 'static/singapore_administrative_region_boundaries.geojson'
 
-  m.add_geojson(regions, layer_name='US Regions')
+  m.add_geojson(regions, layer_name='SG Regions')
   m.add_points_from_xy(
       cities,
       x="longitude",
       y="latitude",
-      color_column='region',
+      #color_column='region',
       icon_names=['gear', 'map', 'leaf', 'globe'],
       spin=True,
       add_legend=True,
